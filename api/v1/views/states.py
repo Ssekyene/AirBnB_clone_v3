ed065a589ba79ecbc6abb3ef207bedfe297aa8d5
Ssekyene Robert <robertssekyene05@gmail.com>
2024-08-24 12:59:56 +0000

    made some patches on

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,14 +16,14 @@ from models.state import State
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_state():
     """Retrieves list of all State object: GET /api/v1/states"""
-    state_list = [state.to_dict() for state in storage.all('State').values()]
+    state_list = [state.to_dict() for state in storage.all(State).values()]
     return jsonify(state_list)
 
 
 @app_views.route('states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state_id(state_id):
     """Retrieves a State object: GET /api/v1/states/<state_id>"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     return jsonify(state.to_dict())
@@ -32,7 +32,7 @@ def get_state_id(state_id):
 @app_views.route('states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object:: DELETE /api/v1/states/<state_id>"""
-    state = storage.get("State", state_id)
+    state = storage.get(State, state_id)
     if state is None:
         abort(404)
     state.delete()
@@ -60,7 +60,7 @@ def update_state(state_id):
     if not request.get_json():
         return jsonify({"error": "Not a JSON"}), 400
 
-    obj = storage.get("State", state_id)
+    obj = storage.get(State, state_id)
     if obj is None:
         abort(404)
     data = request.get_json()
